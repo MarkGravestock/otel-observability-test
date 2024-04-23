85a09139fcbba90514f4aa72229ab9d1c82c76b7
MarkGravestock <mark.gravestock@gmail.com>
2024-04-23 19:59:59 +0100

    Initial commit

---
+++ src/main/java/org/example/observabilitytest/ObservabilityTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.observabilitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObservabilityTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/observabilitytest/OtelLoggingConfig.java
@@ -0,0 +1,2 @@
+package org.example.observabilitytest;public class OtelLoggingConfig {
+}
