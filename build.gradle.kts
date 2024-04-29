4f1ead3b761c4b3165cfafc0595dc74c9d23601b
MarkGravestock <mark.gravestock@gmail.com>
2024-04-29 20:44:09 +0100

    Add docker support and runners.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-docker-compose")
     // Required for @Timed aspect
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("io.micrometer:micrometer-tracing-bridge-otel")
