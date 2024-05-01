dfe4ab230872d7326974b218a841dbcb4d37d190
MarkGravestock <mark.gravestock@gmail.com>
2024-05-01 21:40:36 +0100

    Strip out all manual configuration for auto configure.

--- src/main/java/org/example/observabilitytest/GreetingController.java
+++ src/main/java/org/example/observabilitytest/GreetingController.java
@@ -1,9 +1,6 @@
 package org.example.observabilitytest;
 
 
-import io.micrometer.core.annotation.Timed;
-import io.micrometer.observation.Observation;
-import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -13,33 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 @RestController
-@Timed
 @RequiredArgsConstructor
 @Slf4j
 @Profile("greeting")
 public class GreetingController {
 
-    private final ObservationRegistry observationRegistry;
     private final RestClient.Builder restClientBuilder;
 
-    @Timed(value = "greeting.api.timing")
     @GetMapping("/greeting")
     String message(@RequestParam("name")String name) {
-        RestClient client = restClientBuilder.observationRegistry(observationRegistry).build();
+        RestClient client = restClientBuilder.build();
 
         log.info("Generating greeting");
 
-        var salutation = Observation.createNotStarted("calling.salutation.service", this.observationRegistry).observe(() -> client.get()
+        var salutation = client.get()
                 .uri("http://localhost:8081/salutation")
                 .retrieve()
                 .toEntity(String.class)
-                .getBody());
+                .getBody();
 
-        var visitNumber = Observation.createNotStarted("calling.visitor.service", this.observationRegistry).observe(() -> client.get()
+        var visitNumber = client.get()
                 .uri("http://localhost:8082/permanent-visit")
                 .retrieve()
                 .toEntity(Long.class)
-                .getBody());
+                .getBody();
 
         return String.format("%s, %s. You are our %d visitor",salutation, name, visitNumber);
     }
--- src/main/java/org/example/observabilitytest/VisitorController.java
+++ src/main/java/org/example/observabilitytest/VisitorController.java
@@ -1,6 +1,5 @@
 package org.example.observabilitytest;
 
-import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
@@ -8,17 +7,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
-@Timed
 @Slf4j
 @Profile("visitor")
 public class VisitorController {
 
     private final Counter visitCounter;
     private final VisitorRepository visitorRepository;
-    private final AtomicLong visitorCount = new AtomicLong();
 
     public VisitorController(MeterRegistry meterRegistry, VisitorRepository visitorRepository) {
         this.visitorRepository = visitorRepository;
@@ -26,20 +21,6 @@ public class VisitorController {
     }
 
 
-    @Timed(value = "visit.api.timing")
-    @GetMapping("/visit")
-    Long visit() {
-
-        var visitNumber = visitorCount.incrementAndGet();
-
-        visitCounter.increment();
-
-        log.info("Visit number {}", visitNumber);
-
-        return visitNumber;
-    }
-
-    @Timed(value = "permanent.visit.api.timing")
     @GetMapping("/permanent-visit")
     Long permanentVisit() {
         long counterId = 1L;
