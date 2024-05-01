68ea70a328f54f96038180fb0270ab92498c0dea
MarkGravestock <mark.gravestock@gmail.com>
2024-05-01 20:13:32 +0100

    Update gradlew and disable test

--- src/test/java/org/example/observabilitytest/ObservabilityTestApplicationTests.java
+++ src/test/java/org/example/observabilitytest/ObservabilityTestApplicationTests.java
@@ -1,9 +1,11 @@
 package org.example.observabilitytest;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class ObservabilityTestApplicationTests {
 
     @Test
