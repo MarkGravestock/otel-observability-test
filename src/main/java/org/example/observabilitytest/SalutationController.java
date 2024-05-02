12948ed99cf8381657cd63511af7c4db57404727
MarkGravestock <mark.gravestock@gmail.com>
2024-05-02 20:06:11 +0100

    Tidy up.

--- src/main/java/org/example/observabilitytest/SalutationController.java
+++ src/main/java/org/example/observabilitytest/SalutationController.java
@@ -2,15 +2,12 @@ package org.example.observabilitytest;
 
 
 import io.micrometer.core.annotation.Timed;
-import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 
 import java.time.LocalTime;
 
