0a45e57b089814b87f783b36b972c1fdd1c75721
MarkGravestock <MarkGravestock@users.noreply.github.com>
2026-03-26 01:06:36 +0000

    Update to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,39 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
+    alias(libs.plugins.spring.boot)
 }
 
 group = "org.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
-
-repositories {
-    mavenCentral()
-}
-
-val otelVersion = "2.3.0-alpha"
-
-dependencyManagement {
-    imports {
-        mavenBom("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:${otelVersion}")
-    }
-}
-
-val mySqlConnectorVersion = "8.0.28"
 
 dependencies {
-    compileOnly("org.projectlombok:lombok")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
-    implementation("io.opentelemetry.instrumentation:opentelemetry-logback-appender-1.0")
-    runtimeOnly("mysql:mysql-connector-java:${mySqlConnectorVersion}")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Native Gradle BOM support. Non-inheriting configurations each declare
+    // the platform explicitly so version constraints are applied uniformly.
+    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
+    implementation(platform(libs.otel.bom))
+    annotationProcessor(platform(SpringBootPlugin.BOM_COORDINATES))
+    developmentOnly(platform(SpringBootPlugin.BOM_COORDINATES))
+    testImplementation(platform(SpringBootPlugin.BOM_COORDINATES))
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.data.jpa)
+    runtimeOnly(libs.spring.boot.docker.compose)
+    implementation(libs.otel.logback.appender)
+    runtimeOnly(libs.mysql.connector.j)
+    developmentOnly(libs.spring.boot.devtools)
+    testImplementation(libs.spring.boot.starter.test)
 }
 
 tasks.withType<Test> {
--- src/main/java/org/example/observabilitytest/ObservabilityTestApplication.java
+++ src/main/java/org/example/observabilitytest/ObservabilityTestApplication.java
@@ -2,13 +2,19 @@ package org.example.observabilitytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class ObservabilityTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ObservabilityTestApplication.class, args);
+    }
+
+    @Bean
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 
 }
